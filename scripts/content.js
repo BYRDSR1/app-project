7e587342d122f1ebd9daec6ec394fb8bb3dec087
Spencer Byrd <byrdsr1@student.okaloosaschools.com>
2021-09-21 12:55:35 -0400

    work on app

---
+++ scripts/content.js
@@ -0,0 +1,24 @@
+const content = {
+
+	pictureContent: "",
+	homeContent: "",
+	videoContent: "",
+
+
+	switchPage = (page) => {
+		const wrapper = document.getElementById("outer-wrapper");
+		
+		//Remove any content from wrapper if it exists
+		if(page == "homepage") {
+			//add this.homeContent to wrapper
+		} else if(page == "videopage") {
+			//add this.videoContent
+		} else if(page == "pictureContent") {
+			//add this.pictureContent 
+	}
+
+
+
+
+
+}
