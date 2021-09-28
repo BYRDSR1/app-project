77740f57051f3e5fefd93a7fa47db797bae2ac0c
Spencer Byrd <spencerlink06@gmail.com>
2021-09-27 19:37:12 -0500

    work on video page

--- scripts/video-colors.js
+++ scripts/video-colors.js
@@ -1,9 +1,10 @@
 const videoSetup = async () => {
 	if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-		const wrapper = document.getElementById("wrapper");
+		const wrapper = document.getElementById("video-main");
 		const video = document.createElement("video");
 		video.id = "video";
-		video.width = video.height = 640;
+		video.width = documment.documentElement.clientWidth;
+		video.height = document.documentElement.clientHeight;
 		video.autoplay = true;
 		wrapper.appendChild(video);
 	}
